839ed35201210af01e18c82955077e42dad17042
raphaelfontenelle <42190965+raphaelfontenelle@users.noreply.github.com>
2022-02-26 10:07:12 -0300

    Add files via upload

---
+++ CH2/EchoServerHandle/src/main/java/com/sicpa/server/EchoServerHandler.java
@@ -0,0 +1,27 @@
+package com.sicpa.server;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+import java.nio.ByteBuffer;
+
+@ChannelHandler.Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg){
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println(
+                "Server received: " + in.toString(CharsetUtil.UTF_8));
+        ctx.write(in);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause){
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
